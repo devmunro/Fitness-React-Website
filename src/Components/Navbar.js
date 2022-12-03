975920f34d0b2a8e584273685e730fbac4e283d9
David Munro <defmunro@gmail.com>
2022-12-03 15:15:26 +0000

    add Navbar styles and logo

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {Stack} from '@mui/material'
+import Logo from '../assets/assets/images/Logo.png'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <Stack
+    direction="row"
+    justifyContent="space-between"
+    sx={{gap:{sm:'122px', xs:'40px'}, mt: {sm:"32px", xl:"20px"}}}>
+        <Link to="/">
+        <img src={Logo} alt="Gym Logo of a muscle flex" style={{width: '48px', height: '48px', margin: '0 20px'}}></img>
+        </Link>
+<Stack
+direction="row"
+gap="40px"
+fontSize="24px"
+alignItems="flex-end">
+    <Link to="/" style={{textdecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625'}}>Home</Link>
+    <a href="#exercises" style={{textdecoration: 'none', color: '#3A1212'}}> Exercises</a>
+</Stack>
+    </Stack>
+    
   )
 }
 
