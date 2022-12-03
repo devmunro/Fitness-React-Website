db81c13a961f4511759cff42149812fee6f9d8c7
David Munro <defmunro@gmail.com>
2022-12-03 15:17:11 +0000

    Finish Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,7 +8,7 @@ function Navbar() {
     <Stack
     direction="row"
     justifyContent="space-around"
-    sx={{gap:{sm:'122px', xs:'40px'}, mt: {sm:"32px", xl:"20px"}}}>
+    sx={{gap:{sm:'122px', xs:'40px'}, mt: {sm:"32px", xl:"20px"}, justifyContent: 'none'}} px="20px">
         <Link to="/">
         <img src={Logo} alt="Gym Logo of a muscle flex" style={{width: '48px', height: '48px', margin: '0 20px'}}></img>
         </Link>
