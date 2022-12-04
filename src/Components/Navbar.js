c9c72fa2f0e62a8251bd10a94a89bf7668b84aa8
David Munro <defmunro@gmail.com>
2022-12-04 16:07:11 +0000

    add exercises gifs

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,7 +8,8 @@ function Navbar() {
     <Stack
     direction="row"
     justifyContent="space-around"
-    sx={{gap:{sm:'122px', xs:'40px'}, mt: {sm:"32px", xl:"20px"}, justifyContent: 'none'}} px="20px">
+    backgroundColor="#110920"
+    sx={{gap:{sm:'122px', xs:'40px'}, justifyContent: 'none'}} px="20px" width="100%">
         <Link to="/">
         <img src={Logo} alt="Gym Logo of a muscle flex" style={{width: '48px', height: '48px', margin: '0 20px'}}></img>
         </Link>
@@ -17,8 +18,8 @@ direction="row"
 gap="40px"
 fontSize="24px"
 alignItems="flex-end">
-    <Link to="/" style={{textdecoration: 'none', color: '#3A1212', borderBottom: '3px solid #FF2625'}}>Home</Link>
-    <a href="#exercises" style={{textdecoration: 'none', color: '#3A1212'}}> Exercises</a>
+    <Link to="/" style={{textdecoration: 'none', color: 'white', borderBottom: '3px solid #FF2625'}}>Home</Link>
+    <a href="#exercises" style={{textdecoration: 'none', color: 'white'}}> Exercises</a>
 </Stack>
     </Stack>
     
