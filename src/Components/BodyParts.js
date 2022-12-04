0c8be7ed37eda97c346ef44abb072bca9b82f026
David Munro <defmunro@gmail.com>
2022-12-04 14:48:37 +0000

    add icons to each body part

--- src/Components/BodyParts.js
+++ src/Components/BodyParts.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { Stack, Typography } from "@mui/material";
-import Icon from "../assets/assets/icons/gym.png";
+import Chest from "../assets/assets/icons/bodyParts/chest.png";
+import Back from "../assets/assets/icons/bodyParts/back.png";
+import Main from "../assets/assets/icons/bodyParts/main.png";
+import Cardio from "../assets/assets/icons/bodyParts/cardio.png";
+import LowerArms from "../assets/assets/icons/bodyParts/lowerarm.png";
+import LowerLegs from "../assets/assets/icons/bodyParts/lowerleg.png";
+import Neck from "../assets/assets/icons/bodyParts/neck.png";
+import Shoulders from "../assets/assets/icons/bodyParts/shoulders.png";
+import UpperArms from "../assets/assets/icons/bodyParts/upperarm.png";
+import UpperLegs from "../assets/assets/icons/bodyParts/upperleg.png";
+import Waist from "../assets/assets/icons/bodyParts/abs.png";
 
 export default function BodyParts({ item, setBodyPart, bodyPart }) {
+  console.log(item);
   return (
     <Stack
       type="button"
@@ -20,10 +31,30 @@ export default function BodyParts({ item, setBodyPart, bodyPart }) {
       }}
     >
       <img
-        src={Icon}
+        src={
+          (item === "chest" && Chest) ||
+          (item === "back" && Back) ||
+          (item === "all" && Main) ||
+          (item === "cardio" && Cardio) ||
+          (item === "lower legs" && LowerLegs) ||
+          (item === "lower arms" && LowerArms) ||
+          (item === "neck" && Neck) ||
+          (item === "shoulders" && Shoulders) ||
+          (item === "upper arms" && UpperArms) ||
+          (item === "upper legs" && UpperLegs) ||
+          (item === "waist" && Waist)
+        }
         alt="gym dumbbell icon"
-        styles={{ width: "40px", height: "40px" }}
+        style={{ width: "60px", height: "60px" }}
       ></img>
+      <Typography
+        fontSize="24px"
+        fontWeight="bold"
+        color="#3A1212"
+        textTransform="capitalize"
+      >
+        {item}
+      </Typography>
     </Stack>
   );
 }
