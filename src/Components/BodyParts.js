ff29b60827376165d70b709557ed1df43e85dc77
David Munro <defmunro@gmail.com>
2022-12-04 15:14:34 +0000

    adjusted arrows part 2

--- src/Components/BodyParts.js
+++ src/Components/BodyParts.js
@@ -20,14 +20,15 @@ export default function BodyParts({ item, setBodyPart, bodyPart }) {
       justifyContent="center"
       className="bodyPart-card"
       sx={{
-        borderTop: bodyPart === item ? "4px solid #FF265" : "",
-        backgroundColor: "#FFF",
+        borderTop: bodyPart === item && "24px solid black",
+        backgroundColor: "lightGray",
         borderBottomLeftRadius: "20px",
         width: "270px",
         height: "280px",
         cursor: "pointer",
         gap: "47px",
       }}
+      
     >
       <img
         src={
@@ -51,6 +52,8 @@ export default function BodyParts({ item, setBodyPart, bodyPart }) {
         fontWeight="bold"
         color="#3A1212"
         textTransform="capitalize"
+        onClick={() => { setBodyPart(item)
+        window.scrollTo({top:1800, left:100, behaniour:"smooth"})}}
       >
         {item}
       </Typography>
