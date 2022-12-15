834e8c2ef033d44ea913d0959467f4b06e98a5e6
David Munro <defmunro@gmail.com>
2022-12-15 15:58:38 +0000

    fix scroll error

--- src/Components/SideScrollBar.js
+++ src/Components/SideScrollBar.js
@@ -27,11 +27,10 @@ const RightArrow = () => {
 
 const SideScrollbar = ({ data, setBodyPart, bodyPart }) => (
   <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
-    
     {data.map((item) => (
       <Box
         key={item.id || item}
-        itemID={item.id || item}
+        itemId={item.id || item}
         title={item.id || item}
         m="0 40px"
       >
